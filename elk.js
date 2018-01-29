9a2e5058175fe190ad026b71e9fdd811840d1123
Charles Yang <charles.y.yang@oracle.com>
2018-01-29 17:05:50 -0500

    js

--- elk.js
+++ elk.js
@@ -21,11 +21,57 @@
         setTrackId(trackId);
       }
 
-      preHook();
-      trackPageView();
-      postHook();
+      var data = grabVanillaVisitorsData();
+
+      preHook(data);
+      trackPageView(data);
+      postHook(data);
     },
+
+    // Allow different way of storing cookies
+    readTrackId() {
+      readCookie('GUID');
+    },
+
+    // Allow different way of setting cookies
+    setTrackId(trackId) {
+      setCookie(trackId);
+    },
+
+    // Allow different way of generating the cookie
+    generateTrackId() {
+      generateRandomTrackIdFromClient();
+    }
   };
+
+  function generateRandomTrackIdFromClient() {
+
+  }
+
+  function setCookie(trackId) {
+
+  }
+
+  function readCookie(cookieName) {
+    var cookies=document.cokie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookieValue=getCookieValues(cookies[i],cookieName);
+      if (!!cookieValue) {
+        return unescape(cookieValue);
+      }
+    }
+  }
+
+  function getCookieValues(cookieString, cookieName) {
+    if (cookieString==='') { return null; }
+    var posEquals=cookieString.indexOf('=');
+    if (posEquals >0) {
+      var extractedName=cookieString.substr(0, posEquals);
+      if (extractedName.trim() === cookieName) {
+        return cookieName.substr(posEquals+1);
+      }
+    }
+  }
 
   var _windowElk = window._elk;
   window._elk = new Elk();
